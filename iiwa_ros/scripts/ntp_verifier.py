575de5f5944879a6d5df610e6d44408bfce57bf3
lyh458 <liyihui.ing@gmail.com>
2020-11-27 19:42:56 +0800

    Add yajun's experiments

--- iiwa_ros/scripts/ntp_verifier.py
+++ iiwa_ros/scripts/ntp_verifier.py
@@ -2,7 +2,7 @@
 
 import rospy
 from sensor_msgs.msg import JointState
-from iiwa_msgs.msg import CartesianPose
+import geometry_msgs
 
 rospy.init_node('ntp_verifier')
 
@@ -13,11 +13,11 @@ def ntplog(msg):
 	last_msg = msg
 
 def print_msg(msg):
-  if msg is not None:
-    diff = msg.poseStamped.header.stamp - rospy.Time.now()
-    print('The current time difference is: {} secs'.format(diff.to_sec()))
+	if msg is not None:
+		diff = msg.header.stamp - rospy.Time.now()
+		print('time difference: {} secs'.format(diff.to_sec()))
 
-sub = rospy.Subscriber('/iiwa/state/CartesianPose', CartesianPose, ntplog, queue_size=1)
+sub = rospy.Subscriber('/iiwa/state/CartesianPose', geometry_msgs.msg.PoseStamped, ntplog, queue_size=1)
 
 while not rospy.is_shutdown():
 	print_msg(last_msg)
